01813c5826985d5196027c2bf550b0e32e55e34f
Michael Fahy <fahy@chapman.edu>
2019-09-28 16:14:04 -0700

    Updates for Python 3

--- twitterexample.py
+++ twitterexample.py
@@ -11,6 +11,8 @@
 
 import twitter
 import json
+import sys
+import codecs
 
 # XXX: Go to http://dev.twitter.com/apps/new to create an app and get values
 # for these credentials, which you'll need to provide in place of these
@@ -18,27 +20,30 @@ import json
 # See https://dev.twitter.com/docs/auth/oauth for more information
 # on Twitter's OAuth implementation.
 
-print 'Example 1'
-print 'Establish Authentication Credentials'
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+print('Example 1')
+print('Establish Authentication Credentials')
+
 CONSUMER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxx'
 CONSUMER_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 OAUTH_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 OAUTH_TOKEN_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,
                            CONSUMER_KEY, CONSUMER_SECRET)
 
 twitter_api = twitter.Twitter(auth=auth)
 
-print "Nothing to see by displaying twitter_api"
-print " except that it's now a defined variable"
-print
-print twitter_api
-print
-raw_input('Press Enter to see Example 2')
-print
+print("Nothing to see by displaying twitter_api")
+print(" except that it's now a defined variable")
+print()
+print(twitter_api)
+print()
+input('Press Enter to see Example 2')
+print()
 
-print "---------------------------------------------------------------------"
-print 'Example 2 - Display world and US trends'
+print("---------------------------------------------------------------------")
+print('Example 2 - Display world and US trends')
 # The Yahoo! Where On Earth ID for the entire world is 1.
 # See https://dev.twitter.com/docs/api/1.1/get/trends/place and
 # http://developer.yahoo.com/geo/geoplanet/
@@ -52,35 +57,35 @@ US_WOE_ID = 23424977
 
 world_trends = twitter_api.trends.place(_id=WORLD_WOE_ID)
 us_trends = twitter_api.trends.place(_id=US_WOE_ID)
-print 'Display World Trends'
-print
-print world_trends
-print
-print 'Display US trends'
-print
-print us_trends
-print
-raw_input("Press Enter to see Example 3 ")
-print
-print "---------------------------------------------------------------------"
-print 'Example 3. Displaying API responses as pretty-printed JSON '
-print
-raw_input("Press Enter to see Example 3 ")
-print
-print
-print 'World trends with json.dumps'
-print json.dumps(world_trends, indent=1)
-print
-print
-raw_input("Press Enter to see US trends ")
-print
-print 'US trends with json.dumps'
-print json.dumps(us_trends, indent=1)
-print
-raw_input("Press Enter to see Example 4 ")
-print
-print "---------------------------------------------------------------------"
-print 'Example 4. Computing the intersection of two sets of trends'
+print('Display World Trends')
+print()
+print(world_trends)
+print()
+print('Display US trends')
+print()
+print(us_trends)
+print()
+input("Press Enter to see Example 3 ")
+print()
+print("---------------------------------------------------------------------")
+print('Example 3. Displaying API responses as pretty-printed JSON ')
+print()
+input("Press Enter to see Example 3 ")
+print()
+print()
+print('World trends with json.dumps')
+print(json.dumps(world_trends, indent=1))
+print()
+print()
+input("Press Enter to see US trends ")
+print()
+print('US trends with json.dumps')
+print(json.dumps(us_trends, indent=1))
+print()
+input("Press Enter to see Example 4 ")
+print()
+print("---------------------------------------------------------------------")
+print('Example 4. Computing the intersection of two sets of trends')
 world_trends_set = set([trend['name']
                         for trend in world_trends[0]['trends']])
 
@@ -89,14 +94,14 @@ us_trends_set = set([trend['name']
 
 common_trends = world_trends_set.intersection(us_trends_set)
 
-print common_trends
-print
-print
-print "---------------------------------------------------------------------"
-print 'Example 5. Collecting search results'
-print
-raw_input("Press Enter to see Example 5 ")
-print
+print(common_trends)
+print()
+print()
+print("---------------------------------------------------------------------")
+print('Example 5. Collecting search results')
+print()
+input("Press Enter to see Example 5 ")
+print()
 # Import unquote to prevent url encoding errors in next_results
 
 
@@ -106,10 +111,10 @@ print
 # and is used throughout the remainder of this chapter.
 
 # q = '#MentionSomeoneImportantForYou'
-q = raw_input('Enter a search term: ')
+q = input('Enter a search term: ')
 
 # print q
-# raw_input("Press Enter to continue")
+# input("Press Enter to continue")
 
 count = 1000
 
@@ -122,10 +127,11 @@ statuses = search_results['statuses']
 # Iterate through 5 more batches of results by following the cursor
 
 for _ in range(5):
-    print "Length of statuses", len(statuses)
+    print("Length of statuses", len(statuses))
     try:
         next_results = search_results['search_metadata']['next_results']
-    except KeyError, e:  # No more results when next_results doesn't exist
+    # except KeyError, e:  # No more results when next_results doesn't exist
+    except KeyError:
         break
 
     # Create a dictionary from next_results, which has the following form:
@@ -136,16 +142,16 @@ for _ in range(5):
     statuses += search_results['statuses']
 
 # Show one sample search result by slicing the list...
-print json.dumps(statuses[0], indent=1)
+print(json.dumps(statuses[0], indent=1))
 
-print
-raw_input("Press Enter to see Example 6 ")
-print
-print "---------------------------------------------------------------------"
-print 'Example 6. Extracting text, screen names, and hashtags from tweets'
-print
-raw_input("Press Enter to see Example 6 ")
-print
+print()
+input("Press Enter to see Example 6 ")
+print()
+print("---------------------------------------------------------------------")
+print('Example 6. Extracting text, screen names, and hashtags from tweets')
+print()
+input("Press Enter to see Example 6 ")
+print()
 status_texts = [status['text']
                 for status in statuses]
 
@@ -164,16 +170,16 @@ words = [w
 
 # Explore the first 5 items for each...
 
-print json.dumps(status_texts[0:5], indent=1)
-print json.dumps(screen_names[0:5], indent=1)
-print json.dumps(hashtags[0:5], indent=1)
-print json.dumps(words[0:5], indent=1)
+print(json.dumps(status_texts[0:5], indent=1))
+print(json.dumps(screen_names[0:5], indent=1))
+print(json.dumps(hashtags[0:5], indent=1))
+print(json.dumps(words[0:5], indent=1))
 
-print
-raw_input("Press Enter to see Example 7 ")
-print
-print "---------------------------------------------------------------------"
-print 'Example 7. Calculating lexical diversity for tweets'
+print()
+input("Press Enter to see Example 7 ")
+print()
+print("---------------------------------------------------------------------")
+print('Example 7. Calculating lexical diversity for tweets')
 
 
 # A function for computing lexical diversity
@@ -187,20 +193,20 @@ def average_words(statuses):
     return 1.0*total_words/len(statuses)
 
 
-print 'Lexical diversity of words: '
-print lexical_diversity(words)
-print 'Lexical diversity of screen names: '
-print lexical_diversity(screen_names)
-print 'Lexical diversity of hashtags: '
-print lexical_diversity(hashtags)
-print 'Average number of words per tweet: '
-print average_words(status_texts)
+print('Lexical diversity of words: ')
+print(lexical_diversity(words))
+print('Lexical diversity of screen names: ')
+print(lexical_diversity(screen_names))
+print('Lexical diversity of hashtags: ')
+print(lexical_diversity(hashtags))
+print('Average number of words per tweet: ')
+print(average_words(status_texts))
 
-print
-raw_input("Press Enter to see Example 8 ")
-print
-print "---------------------------------------------------------------------"
-print 'Example 8. Looking up users who have retweeted a status'
+print()
+input("Press Enter to see Example 8 ")
+print()
+print("---------------------------------------------------------------------")
+print('Example 8. Looking up users who have retweeted a status')
 
 
 # Get the original tweet id for a tweet from its retweeted_status node
@@ -208,15 +214,15 @@ print 'Example 8. Looking up users who have retweeted a status'
 # from the text of the book
 
 _retweets = twitter_api.statuses.retweets(id=317127304981667841)
-print [r['user']['screen_name'] for r in _retweets]
+print([r['user']['screen_name'] for r in _retweets])
 
 
-print
-raw_input("Press Enter to see Example 9 ")
-print
+print()
+input("Press Enter to see Example 9 ")
+print()
 
-print "---------------------------------------------------------------------"
-print 'Example 9. Sentiment Analysis on the search term from Example 5'
+print("---------------------------------------------------------------------")
+print('Example 9. Sentiment Analysis on the search term from Example 5')
 sent_file = open('AFINN-111.txt')
 
 scores = {}  # initialize an empty dictionary
@@ -229,6 +235,6 @@ for line in sent_file:
 score = 0
 for word in words:
     uword = word.encode('utf-8')
-    if uword in scores.keys():
+    if word in scores.keys():
         score = score + scores[word]
-print float(score)
+print(float(score))
